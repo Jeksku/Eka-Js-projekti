465ad1f55c44115e740daa603765860de924458e
unknown <tomas.roulamo@edu.keuda.fi>
2020-08-13 10:43:48 +0300

    Tehtävä 1

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var etunimi = 'Tomas';
 var sukunimi = 'Roulamo';
 var ika = '17';
+var postinro = '01450'
+var puhelinnro = '0452033707'
+var lahiosoite = 'Hirvitie 25 C A 2'
 alert ('Minun nimeni on' + ' ' + etunimi + ' ' + sukunimi + ' ' + 'ja ikäni on' + ' ' + ika + '.');
-console.log ('Muutettu234567')
+alert ('Minun osoitteeni on' + ' ' + lahiosoite + ' ' + postinro + '. ' + 'Ja minun puhelinnumero on' + ' ' + puhelinnro + '.')
