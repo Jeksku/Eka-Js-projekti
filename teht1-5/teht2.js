beca3ebfd55d02fdc1a4b5bc627e84e5f25c5bba
unknown <tomas.roulamo@edu.keuda.fi>
2020-08-20 11:23:12 +0300

    teht 1-5 kansio luotu ja teht 2 kesken to 20.8

---
+++ teht1-5/teht2.js
@@ -0,0 +1,12 @@
+function JarjestaLuvut(){
+    var luku1, luku2, luku3, luku4, luku5;
+    luku1 = document.getElementById('eka').value;
+    luku2 = document.getElementById('toka').value;
+    luku3 = document.getElementById('kolmas').value;
+    luku4 = document.getElementById('neljas').value;
+    luku5 = document.getElementById('viides').value;
+
+    if (luku1 > luku2 && luku3 && luku4 && luku5 ) {
+        
+    }
+}
