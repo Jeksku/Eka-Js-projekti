8a831db7499507112dcf8b7ef14db2632c521dbc
unknown <tomas.roulamo@edu.keuda.fi>
2020-08-20 10:07:00 +0300

    teht 5 ja 4 to 20.8

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var sekunneiksi = tunnit * 60 + minuutit + sekunnit * 60;*/
     tun = parseInt(tun);
     min = parseInt(min);
     document.write('Tähän mennessä tänään minuutteja on kulunut ' + laske + '.');
-}
+}*/
 
 function vertaile()
 {
@@ -56,7 +56,7 @@ function jaa()
     luku1 = parseInt(document.getElementById('eka').value);
     luku2 = parseInt(document.getElementById('toka').value);
     document.write("Lukujen jako on: " + (luku1 / luku2));
-}*/
+}
 
 function JarjestaLuvut()
 {
@@ -66,5 +66,6 @@ function JarjestaLuvut()
     luku3 = parseInt(document.getElementById('kolmas').value);
     
 }
+
 
 
---
+++ teht5.js
@@ -0,0 +1,17 @@
+function käännä() 
+{
+    var muuttuja;
+    muuttuja = document.getElementById('kielet').value;
+    if (muuttuja = "sve")
+    {
+        document.write ("Hej världen");
+    } 
+    else if (muuttuja = "eng")
+    {
+        document.write ("Hello world");
+    } 
+    else
+    {
+        document.write ("Hola Mundo")
+    }
+}
