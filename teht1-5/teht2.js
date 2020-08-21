6d90ba588369c70fffd6b9e926d224fe1fa49b6f
unknown <tomas.roulamo@edu.keuda.fi>
2020-08-21 10:47:22 +0300

    teht 2 melkein valmis

--- teht1-5/teht2.js
+++ teht1-5/teht2.js
@@ -1,4 +1,4 @@
-function JarjestaLuvut(){
+function EtsiSuurin(){
     var luku1, luku2, luku3, luku4, luku5;
     luku1 = document.getElementById('eka').value;
     luku2 = document.getElementById('toka').value;
@@ -7,6 +7,22 @@ function JarjestaLuvut(){
     luku5 = document.getElementById('viides').value;
 
     if (luku1 > luku2 && luku3 && luku4 && luku5 ) {
-        
+        document.write (luku1)
+    }
+
+    else if (luku2 > luku3 && luku4 && luku5 ) {
+        document.write (luku2)
+    }
+
+    else if (luku3 > luku4 && luku5 ) {
+        document.write (luku3)
+    }
+
+    else if (luku4 > luku5 ) {
+        document.write (luku4)
+    }
+
+    else {
+        document.write (luku5)
     }
 }
