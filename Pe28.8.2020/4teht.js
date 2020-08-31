a9156d2caf55a231e58a85490d529d18b55a7fa9
Jeksku <tomas.roulamo@hotmail.com>
2020-08-31 09:26:10 +0300

    teht 4 yritys

--- Pe28.8.2020/4teht.js
+++ Pe28.8.2020/4teht.js
@@ -1,8 +1,28 @@
 function Annakortit(){
+    var kasi = ['']
     var maat = ['&#9828;', '&#9827;', '&#9826;', '&#9825;'];
-    var kasi = [];
+    var nrot = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+    laskuri = 0;
 
-    for(var pakka = 2) {
+    for(K = 0; < maat.length; K++) {
 
+        for(L = 0; nrot.length; L++){
+
+            if(K == maat.length-1 && L == nrot.length-1){
+                kasi[laskuri] = maat[K] + nrot[L];
+            }
+
+            else{
+                kasi[laskuri] = maat[K]+nrot[L]
+                laskuri++
+            }
+        }
     }
+    var kortit = [];
+    for(m = 0; m < 5; m++){
+
+        temp = kasi[Math.floor(Math.random()*52];
+        kortit[m] = temp;
+    }
+    document.getElementById('kasi').innerHTML = '<p>' + kortit + '</p>'
 }
