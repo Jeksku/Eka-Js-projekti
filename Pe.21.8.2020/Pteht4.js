90078cd631a316a81d582613b6f394c0a36f23c5
unknown <tomas.roulamo@edu.keuda.fi>
2020-09-02 09:18:14 +0300

    dadada

--- Pe.21.8.2020/Pteht4.js
+++ Pe.21.8.2020/Pteht4.js
@@ -1,10 +1,13 @@
 function SumJaKesk(){
-    var luku1, luku2, luku3, luku4, luku5;
-    luku1 = document.getElementById('eka').value;
-    luku2 = document.getElementById('toka').value;
-    luku3 = document.getElementById('kolmas').value;
-    luku4 = document.getElementById('neljäs').value;
-    luku5 = document.getElementById('viides').value;
+    var luku1, luku2, luku3, luku4, luku5, summa, KA
+    luku1 = parseInt(document.getElementById('eka').value);
+    luku2 = parseInt(document.getElementById('toka').value);
+    luku3 = parseInt(document.getElementById('kolmas').value);
+    luku4 = parseInt(document.getElementById('neljäs').value);
+    luku5 = parseInt(document.getElementById('viides').value);
 
-      
+    summa = luku1 + luku2 + luku3 + luku4 + luku5;
+    KA = summa / 5;
+
+    document.write('Antamiesi numeroiden summa on ' + summa + ' ja niiden keskiarvo on ' + KA + '.')
 }
