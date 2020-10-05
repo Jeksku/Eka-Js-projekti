42c332085a5553cc0effb6cd58b18df9cf363b1b
unknown <tomas.roulamo@edu.keuda.fi>
2020-10-05 11:17:18 +0300

    fdsfdsfdsa

--- Pe28.8.2020/4teht.js
+++ Pe28.8.2020/4teht.js
@@ -1,12 +1,12 @@
 function Annakortit(){
-    var kasi = ['']
+    var kasi = [''];
     var maat = ['&#9828;', '&#9827;', '&#9826;', '&#9825;'];
     var nrot = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
     laskuri = 0;
 
     for(var K = 0; K < maat.length; K++) {
 
-        for(var L = 0; nrot.length; L++){
+        for(var L = 0; L < nrot.length; L++){
 
             if(K == maat.length-1 && L == nrot.length-1){
                 kasi[laskuri] = maat[K] + nrot[L];
@@ -18,11 +18,12 @@ function Annakortit(){
             }
         }
     }
+    console.log(kasi);
     var kortit = [''];
     for(var m = 0; m < 5; m++){
 
-        temp = kasi(Math.floor(Math.random()*52));
-        kortit[m] = temp;
+        temp = Math.floor(Math.random()*52);
+        kortit[m] = kasi[temp];
     }
     document.getElementById('kasi').innerHTML = kortit;
 }
