42b8d2a9b00ffa0aaab30965e99814db7348ba4d
unknown <tomas.roulamo@edu.keuda.fi>
2020-10-05 09:51:40 +0300

    teht 1-5 tehty

--- SilmukkaTehtävät1-5/Steht5.js
+++ SilmukkaTehtävät1-5/Steht5.js
@@ -1,4 +1,24 @@
-function muutaSalasanaksi(){
-    var annetus = document.getElementById('Ssana').value;
-    
+function hipHeijjaa(){
+var luvut = '';
+
+for(var i = 1; i <= 100; i++){
+
+    if(i % 3 == 0){
+        luvut += 'Hip ';
+    }
+
+    else if(i % 5 == 0){
+        luvut += 'Heijjaa ';
+    }
+
+    else if (i % 3 == 0 && i % 5 == 0){
+        luvut += 'Hip Heijjaa ';
+    }
+
+    else{
+    luvut += i + ' ';
+    }
+document.getElementById('rivi').innerHTML = luvut;
+}
+
 }
