fc4c88e5108c3b38a51ceb588eb490020d2fa1eb
Jeksku <tomas.roulamo@hotmail.com>
2020-10-09 19:27:41 +0300

    fdsfsdfds

---
+++ SilmukkaTehtävät/Steht11.js
@@ -0,0 +1,19 @@
+function joku(){
+    var eka = document.getElementById('eka').value;
+    var toka = document.getElementById('toka').value;
+    var pariluvut='';
+    var paritonluvut='';
+    var parisumma=0;
+    var paritonsumma=0;
+    var parialku, paritonalku;
+
+    if(eka%2 == 0){
+        parialku = eka;
+    }
+    else{
+        parialku = eka+1;
+    }
+
+    for(var i = )
+
+}
---
+++ SilmukkaTehtävät/Steht9.js
@@ -0,0 +1,10 @@
+function etsiPjaS(){
+    var eka, toka, kolmas, neljas, viides;
+    eka = document.getElementById('eka').value;
+    toka = document.getElementById('toka').value;
+    kolmas = document.getElementById('kolmas').value;
+    neljas = document.getElementById('neljas').value;
+    viides = document.getElementById('viides').value;
+
+    
+}
